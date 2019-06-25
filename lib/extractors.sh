a47ed572295c5983204533362614f91d096a3803
Igor Sharonov <igsha@users.noreply.github.com>
2019-06-25 22:47:48 +0300

    Support deb-package

--- lib/extractors.sh
+++ lib/extractors.sh
@@ -1,24 +1,17 @@
-#!/usr/bin/env bash
-set -e
-which http xmllint ag fzy openssl jq > /dev/null
+which http xmllint ag fzy openssl jq > /dev/null || return 1
 
-rootname()
-{
-    ag --nocolor -o '.*:?//[^/]+' <<< "$1"
-}
-
-parse_http()
-{
-    http "$1" | xmllint --html --xpath "$2" - 2>/dev/null
-}
+rootname(){ ag --nocolor -o '.*:?//[^/]+' <<< "$1"; }
+parse_http(){ http "$1" | xmllint --html --xpath "$2" - 2>/dev/null; }
+parse_array(){ ag -o "$1:\s*\[\K[^\]]+" | tr ',' '\n'; }
 
 moonwalk()
-{
+(
+    set -e
+
     __url=`parse_http "$1" "string(//*[contains(@src,'moonwalk')]/@src)"`
     [[ ! -z "$__url" ]] || return 1
-
-    __season=`http --follow "$__url" "Referer:$1" | ag -o "seasons:\s*\[\K[^\]]+" | tr ',' '\n' | fzy --prompt="Choose season: "`
-    __episode=`http --follow "$__url" "Referer:$1" "season==$__season" | ag -o "episodes:\s*\[\K[^\]]+" | tr ',' '\n' | fzy --prompt="Choose episode: "`
+    __season=`http --follow "$__url" "Referer:$1" | parse_array seasons | fzy --prompt="Choose season: "`
+    __episode=`http --follow "$__url" "Referer:$1" "season==$__season" | parse_array episodes | fzy --prompt="Choose episode: "`
 
     __page=`http --follow "$__url" "Referer:$1" "season==$__season" "episode==$__episode"`
     __ref=`ag -o "ref:\s*['\"]\K[^'\"]+" <<< "$__page"`
@@ -37,21 +30,22 @@ moonwalk()
     __q=`openssl enc -aes-256-cbc -base64 -A -iv $__iv -K $__key <<< "$__json"`
 
     http POST "$__proto$__host:$__port/vs" "ref=$__ref" "q=$__q" "User-Agent:$__user_agent" | jq -r .m3u8
-}
+)
 
 ralode()
-{
+(
+    set -e
+
     __json=`http "$1" | ag -o "RalodePlayer.init\(\K{.*}(?=\))"`
     [[ ! -z "$__json" ]] || return 1
-
-    __json=`sed -e '1s/^/[/' -e '$s/$/]/' <<< "$__json"`
-    __name=`jq -r '.[0] | .[] | .name' <<< "$__json" | fzy --prompt="Select a dubber: "`
+    __json=`sed -e '1s/^/[/' -e '$s/$/]/' <<< "$__json"` # on separate line to force error
+    __name=`jq -r '.[0] | .[] | .name' <<< "$__json" | fzy --prompt="Select a dubber: "` || return 1
 
     declare -A __items
     eval $(jq -r '.[0] | .[] | select(.name == "'"$__name"'") | .items[] | "__items[\"" + .sname + "\"]=" + (.scode | capture("src=(?<a>\"[^\"]+\")") | .a)' <<< "$__json")
 
-    __episode=`printf "%s\n" "${!__items[@]}" | sort -V | tac | fzy --prompt="Select an episode: "`
+    __episode=`printf "%s\n" "${!__items[@]}" | sort -V | tac | fzy --prompt="Select an episode: "` || return 1
     parse_http "$(rootname "$1")${__items["$__episode"]}" "string(//iframe/@src)" | sed 's@^//@http://@'
-}
+)
 
 export extractors=(moonwalk ralode)
